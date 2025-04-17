2e92657c00cdbce58dd3fd65b055298d88dc2325
Eric-Heredia <ericvheredia@gmail.com>
2025-04-17 17:03:31 -0300

    primeras linéas

---
+++ Prácticas-programación/ejercicio-1.py
@@ -0,0 +1,8 @@
+# Programa para buscar el mayor de 3 números
+
+n1= int(input("Ingrese el 1er número: "))
+n2 = int(input("Ingrese el 2do número: "))
+n2 = int(input("Ingrese eñ 3er número: "))
+
+if n1 > n2:
+    print("")
