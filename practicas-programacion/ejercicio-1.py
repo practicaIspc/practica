90f5fbd335d6886d82f29e89aded57ee29520090
MaxgelMax <maxgelmax21@gmail.com>
2025-04-21 14:51:52 -0300

    Añadiendo otra opcion de solucion

--- practicas-programacion/ejercicio-1.py
+++ practicas-programacion/ejercicio-1.py
@@ -12,3 +12,18 @@ elif n2 > n3:
     print(f'El número mayor es el: {n2}')
 else:
     print(f'El número mayor es el: {n3}')
+
+#Opcion 2
+
+numero1 = int(input("Ingrese el primer numero"))
+numero2 = int(input("Ingrese el segundo numero"))
+numero3 = int(input("Ingrese el tercer numero"))
+
+if numero1 > numero2 and numero1 > numero3:
+   print(f"El numero {numero1} es mayor que los tres numeros")
+
+if numero2 > numero1 and numero2 > numero3:
+    print(f"El numero {numero2} es mayor que los tres numeros")
+
+if numero3 > numero1 and numero3 > numero2:
+    print(f"El numero {numero3} es mayor que los tres numeros")
